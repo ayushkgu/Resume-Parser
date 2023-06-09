c615fddb727d2c4e64d90ce77dee3447bd6009db
salma o <salmaomran2@gmail.com>
2023-06-08 20:39:27 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,18 +4,6 @@
 // // TODO: Add SDKs for Firebase products that you want to use
 // // https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAb9_fbfOQ_wv1FJG3oLLf2rztJLnt79eY",
-//   authDomain: "resume-parser-a7584.firebaseapp.com",
-//   projectId: "resume-parser-a7584",
-//   storageBucket: "resume-parser-a7584.appspot.com",
-//   messagingSenderId: "1094348788909",
-//   appId: "1:1094348788909:web:b35c85d323bfd2b45c2224",
-//   measurementId: "G-E9EXPSYX96"
-// };
-
 // // Initialize Firebase
 // const storage =  getStorage(app);
 // export default storage;
